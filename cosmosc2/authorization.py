4f55ddc7aacaf644d09642e526f865d82576b504
van Andel, Gerhard <gvanande@ball.com>
2021-10-13 16:31:44 -0600

    keep it simple logic

--- cosmosc2/authorization.py
+++ cosmosc2/authorization.py
@@ -84,7 +84,7 @@ class CosmosKeycloakAuthorization(CosmosAuthorization):
         current_time = time.time()
         if self.token is None:
             self._make_token()
-        elif self.expires_at < current_time and self.refresh_expires_at < current_time:
+        elif self.refresh_expires_at < current_time:
             self._make_token()
         elif self.expires_at < current_time:
             self._refresh_token()
